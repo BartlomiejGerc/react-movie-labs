813efe50146a3e1059bc9f76bd3daa194ef2e842
BartlomiejGerc <youremail@anyemailwilldo.com>
2026-03-19 23:19:42 +0000

    Full review page.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import MovieReviewPage from "./pages/movieReviewPage";
+
+
 
 const App = () => {
   return (
@@ -17,6 +22,8 @@ const App = () => {
         </li>
       </ul>
       <Routes>
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+
         <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
